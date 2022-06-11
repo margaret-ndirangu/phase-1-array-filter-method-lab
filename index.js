c3647e885d0e0f0a089cee6ee75c59494deae801
Margaret <wanjirundirangu43@gmail.com>
2022-06-11 17:29:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+  function findMatching(drivers, match) {
+    return drivers.filter(
+      (matchingLetters) => matchingLetters.toLowerCase() === match.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(drivers, lettersMatch) {
+    return drivers.filter(
+      (matchingLetters) => matchingLetters.toLowerCase().indexOf(lettersMatch.toLowerCase())===0
+    );
+  }
+  
+  function matchName(drivers, nameMatch) {
+    return drivers.filter((results) =>results.name === nameMatch);
+  }
